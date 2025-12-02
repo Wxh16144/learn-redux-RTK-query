8c33ead3b664fa9b591df32f2537f34f768d9b0c
𝑾𝒖𝒙𝒉 <wxh1220@gmail.com>
2025-12-02 20:11:56 +0800

    chore: deps & demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button } from "antd";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const slogan = `魔法师正在进行最后的仪式，为您带来一项惊艳功能`;
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h3>{slogan}</h3>
+      <code hidden>src/App.tsx</code>
+      <Button type="primary">Hi</Button>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
